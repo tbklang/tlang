0c6311b3eb7a5a6107b03273665a437fea1217aa
Tristan B. Kildaire <deavmi@redxen.eu>
2021-03-02 21:03:55 +0200

    Initial commit

---
+++ source/app.d
@@ -0,0 +1,14 @@
+/**
+* Tristan's programming language
+*
+* This is TOP SECRET code, not for RELEASE!
+* Violators WILL BE PUT UP AGAINST A WALL AND
+* SHOT!
+*/
+
+import std.stdio;
+
+void main()
+{
+	
+}
