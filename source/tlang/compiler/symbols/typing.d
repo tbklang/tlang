11c22c176a6a44df4db18dc554da1d0c2faabef2
Tristan B. Kildaire <deavmi@redxen.eu>
2021-04-16 22:50:53 +0200

    Figured out what to do

---
+++ source/tlang/compiler/symbols/typing.d
@@ -0,0 +1,14 @@
+module compiler.symbols.typing;
+
+// public interface Type
+// {
+//     public 
+// }
+
+// public class PrimitiveType : Type
+// {
+
+// }
+
+/* TODO: Make Container interface (for `getParent` and `getMembers`), then Clazz and PrimitiveType inherit Type, fuck lmao
+/* TODO: Type then ofc Entity (for `name`) */
