c1f28bd80ebb202873d94b56845a249f5e0a4ced
Tristan B. Kildaire <deavmi@redxen.eu>
2021-04-21 23:29:14 +0200

    Code cleanup

--- source/tlang/compiler/symbols/typing.d
+++ source/tlang/compiler/symbols/typing.d
@@ -2,19 +2,6 @@ module compiler.symbols.typing;
 
 import compiler.symbols.data;
 
-// public interface Type
-// {
-//     public 
-// }
-
-// public class PrimitiveType : Type
-// {
-
-// }
-
-/* TODO: Make Container interface (for `getParent` and `getMembers`), then Clazz and PrimitiveType inherit Type, fuck lmao
-/* TODO: Type then ofc Entity (for `name`) */
-
 public class Type : Entity
 {
     /**
@@ -33,7 +20,7 @@ public class Number : Type
 {
     /* Number of bytes (1,2,4,8) */
     private ubyte width;
-    
+
     this(string name)
     {
         super(name);
