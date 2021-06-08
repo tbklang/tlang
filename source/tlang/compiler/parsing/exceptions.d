18f34b5e999572d9fd2d8bf8c4b27d09fdfbc94e
Tristan B. Kildaire <deavmi@redxen.eu>
2021-06-08 10:45:14 +0200

    Fixed missing imports

--- source/tlang/compiler/parsing/exceptions.d
+++ source/tlang/compiler/parsing/exceptions.d
@@ -1,11 +1,19 @@
 module compiler.parsing.exceptions;
 
+import compiler.parsing.core;
+import compiler.lexer;
+import misc.exceptions;
+import compiler.symbols.check;
+import compiler.symbols.data;
+import compiler.lexer : Token;
+
 public class ParserException : TError
 {
     private Parser parser;
 
     this(Parser parser)
     {
+        super("");
         this.parser = parser;
     }
 
