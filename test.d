43891b1dfbc0ef7fc35679f80196200c3d4eeb04
Tristan B. Kildaire <deavmi@disroot.org>
2021-11-02 15:14:03 +0200

    Added testing files

---
+++ test.d
@@ -0,0 +1,40 @@
+module typeChecking3;
+void main()
+{
+
+        asm
+        {
+            mov R15, RSP;
+        }
+        
+asm
+                {
+                    sub RSP, 4;
+                    mov dword ptr [RSP], 69;
+                }
+                
+asm
+                {
+                    sub RSP, 4;
+                    mov dword ptr [RSP], 69;
+                }
+                
+asm
+                {
+                    sub RSP, 4;
+                    mov dword ptr [RSP], 69;
+                }
+                
+
+        asm
+        {
+            mov RSP, R15;
+        }
+        
+
+        int h = -1;
+        h = *((&h)-4);
+        import std.stdio;
+        writeln(h);
+        
+}
---
+++ tets.d
@@ -0,0 +1,11 @@
+void main()
+{
+	
+	asm
+	{
+		mov RAX, 3;
+		push 5;
+//		pop RAX;
+	}	
+}
+
