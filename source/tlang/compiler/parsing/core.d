fca2bac20e922f11325e75b01763b65c942afbe7
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-17 16:34:52 +0200

    🐞️ Bugfix: Function parameter types should use parseTypedDeclaration(onlyType=true) (#4)
    * Praser
    
    - Added TODO in `parseFuncDef(bool)` explaining where we should place the type deriving code
    
    * Parser
    
    - Attempt "Solution 1" of `https://deavmi.assigned.network/git/tlang/tlang/issues/111`
    
    * Parser
    
    - Cleaned up `parseFuncDef()`
    - Also commented-out incorrect code that expected old type code for pointer handling
    
    * Parser
    
    - In `parseFuncDef()` we have removed commented out code which was, as mentioned in the last commit, incorrect code that expected old type code for pointer handling

--- source/tlang/compiler/parsing/core.d
+++ source/tlang/compiler/parsing/core.d
@@ -906,17 +906,9 @@ public final class Parser
             /* Check if the first thing is a type */
             if(getSymbolType(getCurrentToken()) == SymbolType.IDENT_TYPE)
             {
-                /* Get the type (this can be doted) */
-                string type = getCurrentToken().getToken();
-                nextToken();
-
-                /* If it is a star `*` */
-                while(getSymbolType(getCurrentToken()) == SymbolType.STAR)
-                {
-                    // Make type a pointer
-                    type = type~"*";
-                    nextToken();
-                }
+                /* Get the type */
+                TypedEntity bogusEntity = parseTypedDeclaration(false, false, false, true);
+                string type = bogusEntity.getType();
 
                 /* Get the identifier (This CAN NOT be dotted) */
                 expect(SymbolType.IDENT_TYPE, getCurrentToken());
