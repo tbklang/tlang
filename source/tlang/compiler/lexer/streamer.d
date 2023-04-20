0a35acc6bc18fe305e179c04876e9a7b73cbbe9e
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-20 12:08:28 +0200

    Streaming Lexer
    - Created this class; have not implemented any of the methods

---
+++ source/tlang/compiler/lexer/streamer.d
@@ -0,0 +1,11 @@
+module tlang.compiler.lexer.streamer;
+
+import tlang.compiler.lexer.core2 : LexerInterface;
+
+public class StreamingLexer : LexerInterface
+{
+    this(string sourceCode)
+    {
+        
+    }
+}
