87f484841684e9c0f7d3c19d511074c9ea5c204e
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-20 12:08:09 +0200

    Lexer
    - Defined `LexerInterface` as per #105

--- source/tlang/compiler/lexer/core2.d
+++ source/tlang/compiler/lexer/core2.d
@@ -1,6 +1,16 @@
 module tlang.compiler.lexer.core2;
 
+import tlang.compiler.lexer.tokens : Token;
+
 public interface LexerInterface
 {
-    
+    public Token getCurrentToken();
+
+    public void nextToken();
+
+    public void previousToken();
+
+    public void setCursor(ulong);
+
+    public ulong getCursor();
 }
