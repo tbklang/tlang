f0cd4e045d80cf8b424a4a594f926cbf5dab9876
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-20 12:11:40 +0200

    Streaming lexer
    - Added a TODO about how we can ALSO stream from the file

--- source/tlang/compiler/lexer/streamer.d
+++ source/tlang/compiler/lexer/streamer.d
@@ -4,6 +4,10 @@ import tlang.compiler.lexer.core2 : LexerInterface;
 
 public class StreamingLexer : LexerInterface
 {
+    // TODO: Later on, we can make it even take in a File if
+    // ... you want and even stream bit-by-bit from that
+    // ... we'd need to cache that ofc as well to be able
+    // ... to support re-winding the lexer
     this(string sourceCode)
     {
         
