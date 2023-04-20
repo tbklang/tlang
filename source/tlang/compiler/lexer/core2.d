ddac8e8da9d79fd270fdd5403821759cc7715b61
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-20 14:00:16 +0200

    LexerInterface
    - Added documentation to the interface's methods and the interface itself

--- source/tlang/compiler/lexer/core2.d
+++ source/tlang/compiler/lexer/core2.d
@@ -2,17 +2,51 @@ module tlang.compiler.lexer.core2;
 
 import tlang.compiler.lexer.tokens : Token;
 
+/** 
+ * Defines the interface a lexer must provide
+ * such that is can be used to source tokens
+ * from in the parser
+ */
 public interface LexerInterface
 {
+    /** 
+     * Returns the token at the current cursor
+     * position
+     *
+     * Returns: the `Token`
+     */
     public Token getCurrentToken();
 
+    /** 
+     * Moves the cursor one token forward
+     */
     public void nextToken();
 
+    /** 
+     * Moves the cursor one token backwards
+     */
     public void previousToken();
 
-    public void setCursor(ulong);
+    /** 
+     * Sets the position of the cursor
+     *
+     * Params:
+     *   cursor = the new position
+     */
+    public void setCursor(ulong cursor);
 
+    /** 
+     * Retrieves the cursor's current position
+     *
+     * Returns: the position
+     */
     public ulong getCursor();
 
+    /** 
+     * Checks whether more tokens are available
+     * of not
+     *
+     * Returns: true if more tokens are available, false otherwise
+     */
     public bool hasTokens();
 }
