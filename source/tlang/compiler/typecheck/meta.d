07b637f38eda5b8f1e28d3b4dd89f2fe4aa0b162
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-04-24 09:03:38 +0200

    MetaProcessor
    - Added a type-re-writing facility via `typeRewrite(TypedEntity)` which will re-write the types such as `size_t`, `ssize_t` and so forth
    
    Test cases
    
    - Added a test case `meta/types.t` which tests this

--- source/tlang/compiler/typecheck/meta.d
+++ source/tlang/compiler/typecheck/meta.d
@@ -1,6 +1,6 @@
 module tlang.compiler.typecheck.meta;
 
-import tlang.compiler.symbols.data : Statement;
+import tlang.compiler.symbols.data : Statement, TypedEntity;
 import tlang.compiler.symbols.containers : Container;
 import tlang.compiler.symbols.mcro;
 // import tlang.compiler.typecheck.resolution;
@@ -34,6 +34,31 @@ public class MetaProcessor
         foreach(Statement curStmt; stmts)
         {
             gprintln("MetaProcessor: Examining AST node '"~curStmt.toString()~"'...");
+
+            // TODO: Check for any TypedEntity and look for what their type is, if sizet then replace
+            if(cast(TypedEntity)curStmt)
+            {
+                typeRewrite(cast(TypedEntity)curStmt);
+            }
+        }
+    }
+
+    /** 
+     * Re-writes the types for things such as `size_t`, `ssize_t` and so forth
+     *
+     * Params:
+     *   statement = the `TypedEntity` to apply re-writing to
+     */
+    private void typeRewrite(TypedEntity statement)
+    {
+        import std.string : cmp;
+
+        string type = statement.getType();
+        if(type == "size_t")
+        {
+            // FIXME: This is an example re-write, it should actually look up the compiler
+            // ... config and choose the largest unsigned type from there
+            statement.setType("ulong");
         }
     }
 }
---
+++ source/tlang/testing/meta/types.t
@@ -0,0 +1,6 @@
+module meta_types;
+
+size_t function()
+{
+    return 1;
+}
