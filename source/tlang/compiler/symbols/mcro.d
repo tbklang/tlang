3dcdaf0a8f98bf3de596e677b08238d800ce0660
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-05-15 09:25:23 +0200

    Mcro
    - Removed `Sizeof`, we will let it be parsed as a normal `FunctionCall` and then inspect in `MetaProcessor`

--- source/tlang/compiler/symbols/mcro.d
+++ source/tlang/compiler/symbols/mcro.d
@@ -84,34 +84,3 @@ public class Repr : Expression, MStatementSearchable
         return matches;
     }
 }
-
-
-
-// FIXME: Make this inherit from IntegerLiteral and also,
-// make `getLiteral/number` overridable such that we can
-// actually evaluate it depending on the identifier (type)
-// and return the type's size
-public class Sizeof : IntegerLiteral, MTypeRewritable
-{
-    /** 
-     * The identifier to apply sizeof to
-     *
-     * Example: `sizeof(uint)`
-     */
-    private string identifier;
-
-    this(string identifier)
-    {
-        super("SIZEOF_NUMBER_LITERAL_NOT_YET_SET", IntegerLiteralEncoding.UNSIGNED_INTEGER);
-    }
-
-    public void setType(string identifier)
-    {
-        this.identifier = identifier;
-    }
-
-    public string getType()
-    {
-        return identifier;
-    }
-}
