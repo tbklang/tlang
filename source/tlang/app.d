e1ede771c47872f2759dcfafaccf11f2fe4e3ad0
Boyd Kane <33420535+beyarkay@users.noreply.github.com>
2023-05-30 22:09:19 +0200

    Fix bugz

--- source/tlang/app.d
+++ source/tlang/app.d
@@ -11,8 +11,7 @@ import tlang.commandline.args;
 
 void main(string[] args)
 {
-	/* TODO: Replace with something else */
-    writeln("tlang NO_PUBLISH_RELEASE\n");
+    writeln("something else\n");
 
     /* Parse the command-line arguments */
     parseCommandLine(args);
