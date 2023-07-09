18cdbe1ffc928277e2f688d95bc80569ed965be8
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-07-09 12:55:11 +0200

    `core.lexer` (package)
    - Documented all public imports

--- source/tlang/compiler/lexer/core/package.d
+++ source/tlang/compiler/lexer/core/package.d
@@ -3,6 +3,17 @@
  */
 module tlang.compiler.lexer.core;
 
+/** 
+ * Lexer interface definition
+ */
 public import tlang.compiler.lexer.core.lexer;
+
+/** 
+ * Token definition
+ */
 public import tlang.compiler.lexer.core.tokens;
+
+/**
+ * Exception definitions
+ */
 public import tlang.compiler.lexer.core.exceptions;
