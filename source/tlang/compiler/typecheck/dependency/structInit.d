9d962d43d2736327ef2fd32c542aef07d53370c4
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-07-17 21:37:13 +0200

    StructInstanceInit
    - Added new class which represents the instantiation of a struct-typed variable

---
+++ source/tlang/compiler/typecheck/dependency/structInit.d
@@ -0,0 +1,44 @@
+module tlang.compiler.typecheck.dependency.structInit;
+
+import tlang.compiler.symbols.containers : Container, Struct;
+import tlang.compiler.symbols.data : Entity;
+import tlang.compiler.typecheck.dependency.core;
+
+/** 
+ * Represents the instantiation of a struct-typed
+ * variable
+ */
+public final class StructInstanceInit : DNode
+{
+    /** 
+     * Constructs a new `StructInstanceInit`
+     * which represents the instantiation
+     * of a new instance of the provided
+     * `Struct`
+     *
+     * Params:
+     *   dnodegen = the `DNodeGenerator`
+     *   toInit = the `Struct` being instantiated
+     */
+    this(DNodeGenerator dnodegen, Struct toInit)
+    {
+        super(dnodegen, toInit);
+        initName();
+    }
+
+    /** 
+     * Returns the metadata on the struct that is
+     * to be instantiated
+     *
+     * Returns: the metadata as a `Struct`
+     */
+    public Struct getMetadata()
+    {
+        return cast(Struct)getEntity();
+    }
+
+    private void initName()
+    {
+        name ="structInstantiate: "~resolver.generateName(cast(Container)dnodegen.root.getEntity(), cast(Entity)entity);
+    }
+}
