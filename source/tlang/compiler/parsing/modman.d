70da08010b6bbd28721c6b38dba18897b8f2e9ec
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-10-27 12:21:08 +0200

    ModuleManager
    - Added `validate(string searchPath)`
    - Added `validate(string[] searchPaths)`
    - Added a constructor
    
    ModMan (unitests)
    
    - Added a positive unittest

--- source/tlang/compiler/parsing/modman.d
+++ source/tlang/compiler/parsing/modman.d
@@ -1,9 +1,92 @@
 module tlang.compiler.parsing.modman;
 
+import gogga;
+
 /** 
  * Manager for searching for modules etc.
  */
 public final class ModuleManager
 {
+    /** 
+     * The search paths
+     */
+    private string[] searchPaths;
 
+    /** 
+     * Creates a new module manager with the
+     * provided paths of which it should
+     * consider when searching for module
+     * files
+     *
+     * Params:
+     *   searchPaths = the search paths
+     */
+    this(string[] searchPaths)
+    {
+        validate(searchPaths);
+        this.searchPaths = searchPaths;
+    }
+
+    /** 
+     * Validates the given paths, and only
+     * returns a valid verdict if all of
+     * the paths are valid search paths
+     *
+     * Params:
+     *   searchPaths = the search paths
+     * to consider
+     * Returns: `true` if all paths are valid,
+     * `false` otherwise
+     */
+    public static bool validate(string[] searchPaths)
+    {
+        foreach(string searchPath; searchPaths)
+        {
+            if(!validate(searchPath))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /** 
+     * Validates a given path that it is a valid
+     * search path
+     *
+     * Params:
+     *   searchPath = the path to check
+     * Returns: `true` if the search path is valid,
+     * `false` otherwise
+     */
+    public static bool validate(string searchPath)
+    {
+        // Path cannot be empty
+        if(searchPath.length == 0)
+        {
+            return false;
+        }
+
+        import std.file : isDir;
+
+        // It should be a valid directory
+        return isDir(searchPath);
+    }
+}
+
+/**
+ * Tests the static methods of the `ModuleManager`
+ *
+ * In this case positive verdict is expected
+ */
+unittest
+{
+    string[] goodPaths = [
+        "source/tlang/testing",
+        "source/tlang/testing/modules"
+    ];
+
+    bool res = ModuleManager.validate(goodPaths);
+    assert(res);
 }
