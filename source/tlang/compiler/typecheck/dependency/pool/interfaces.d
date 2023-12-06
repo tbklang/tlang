9f36ea44647cfcf06176650ababb6cf26f1409b9
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-12-06 13:27:09 +0200

    IPoolManager
    - Added a templatised variant of the `pool(Statement)` method

--- source/tlang/compiler/typecheck/dependency/pool/interfaces.d
+++ source/tlang/compiler/typecheck/dependency/pool/interfaces.d
@@ -2,6 +2,7 @@ module tlang.compiler.typecheck.dependency.pool.interfaces;
 
 import tlang.compiler.typecheck.dependency.core : DNode;
 import tlang.compiler.symbols.data : Statement;
+import std.traits : isAssignable;
 
 /** 
  * Defines an interface by which
@@ -22,4 +23,23 @@ public interface IPoolManager
      * Returns: the pooled `DNode`
      */
     public DNode pool(Statement statement);
+
+    /** 
+     * Pools the provided AST node
+     * to a dependency node
+     *
+     * This version is templatised
+     * in that it lets you specify
+     * the type of `DNode` you want
+     * to construct (in the case it
+     * does not yet exist in the
+     * pool) and also the type
+     * of AST node going in
+     *
+     * Params:
+     *   entity = the AST node
+     * Returns: the pooled `DNodeType`
+     */
+    public DNodeType poolT(DNodeType, EntityType)(EntityType entity)
+    if(isAssignable!(DNode, DNodeType));
 }
