5d81eeb23b74682fdfed6aeafcf8408ad3f9f5de
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-12-06 11:16:10 +0200

    IPoolManager
    - Added new interface

---
+++ source/tlang/compiler/typecheck/dependency/pool.d
@@ -0,0 +1,22 @@
+module tlang.compiler.typecheck.dependency.pool;
+
+/** 
+ * Defines an interface by which
+ * `Statement`s (i.e. AST nodes)
+ * can be mapped to a `DNode`
+ * and if one does not exist
+ * then it is created on the
+ * first use
+ */
+public interface IPoolManager
+{
+    /** 
+     * Pools the provided AST node
+     * to a dependency node
+     *
+     * Params:
+     *   statement = the AST node
+     * Returns: the pooled `DNode`
+     */
+    public DNode pool(Statement statement);
+}
