ac54002cac90416ca1d7f65ddd17da889117e50c
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-02-26 08:21:48 +0200

    Implementations
    - Added some stub types `LebanonMapper` and `HashMapper`

---
+++ source/tlang/compiler/codegen/mapper/impls.d
@@ -0,0 +1,22 @@
+module tlang.compiler.codegen.mapper.impls;
+
+import tlang.compiler.codegen.mapper.api;
+import tlang.compiler.symbols.data : Entity;
+
+public class LebanonMapper : SymbolMapperV2
+{
+    public string map(Entity item, ScopeType type)
+    {
+        // TODO: Implement me
+        return null;
+    }
+}
+
+public class HashMapper : SymbolMapperV2
+{
+    public string map(Entity item, ScopeType type)
+    {
+        // TODO: Implement me
+        return null;
+    }
+}
