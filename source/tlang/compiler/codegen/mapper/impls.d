4625e0516e608a30e9627b1794932dc709f4fc8f
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-02-26 08:25:34 +0200

    LebanonMapper
    - Implemented `map(Entity item, ScopeType type)`

--- source/tlang/compiler/codegen/mapper/impls.d
+++ source/tlang/compiler/codegen/mapper/impls.d
@@ -1,19 +1,40 @@
 module tlang.compiler.codegen.mapper.impls;
 
 import tlang.compiler.codegen.mapper.api;
+import tlang.compiler.typecheck.core : TypeChecker;
 import tlang.compiler.symbols.data : Entity;
 
 public class LebanonMapper : SymbolMapperV2
 {
+    private TypeChecker tc;
+
+    this(TypeChecker tc)
+    {
+        this.tc = tc;
+    }
+
     public string map(Entity item, ScopeType type)
     {
-        // TODO: Implement me
-        return null;
+        // Generate the root name for this item
+        string absolutePath = tc.getResolver().generateName(tc.getProgram(), item);
+
+        // Replace all `.`'s with underscores
+        import std.string : replace;
+        string mappedSymbol = replace(absolutePath, ".", "_");
+
+        return mappedSymbol;
     }
 }
 
 public class HashMapper : SymbolMapperV2
 {
+    private TypeChecker tc;
+    
+    this(TypeChecker tc)
+    {
+        this.tc = tc;
+    }
+
     public string map(Entity item, ScopeType type)
     {
         // TODO: Implement me
