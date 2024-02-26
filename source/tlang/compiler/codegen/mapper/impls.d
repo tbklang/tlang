94dc6f9c71b2dfb288131f14d9fd608fae78e7eb
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-02-26 08:28:17 +0200

    LebanonMapper
    - Now respects the `type` field for `copeType.GLOBAL`
    - `ScopeType.LOCAL` is still to be implemented

--- source/tlang/compiler/codegen/mapper/impls.d
+++ source/tlang/compiler/codegen/mapper/impls.d
@@ -15,8 +15,19 @@ public class LebanonMapper : SymbolMapperV2
 
     public string map(Entity item, ScopeType type)
     {
-        // Generate the root name for this item
-        string absolutePath = tc.getResolver().generateName(tc.getProgram(), item);
+        string path;
+        if(type == ScopeType.GLOBAL)
+        {
+            // Generate the root name for this item
+            path = tc.getResolver().generateName(tc.getProgram(), item);
+        }
+        else
+        {
+            // TODO: Implement me
+            // TODO: May need to take in a `Container` (for top-level)
+            // path = tc.getResolver().generateName()
+        }
+        
 
         // Replace all `.`'s with underscores
         import std.string : replace;
