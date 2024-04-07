339b8e3b4a0c9d0604180df820c31812a56181a1
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-04-07 12:29:30 +0200

    SymbolMapper
    - Removed old definition

--- source/tlang/compiler/codegen/mapper/core.d
+++ source/tlang/compiler/codegen/mapper/core.d
@@ -5,24 +5,8 @@ import tlang.compiler.symbols.data;
 import std.conv : to;
 import gogga;
 
-/** 
- * SymbolMapper
- *
- * Maps Entity's to consistent but unique symbol
- * names (strings)
- */
-public class SymbolMapper
-{
-    // Used to map names to entities
-    protected TypeChecker tc;
 
-    this(TypeChecker tc)
-    {
-        this.tc = tc;
-    }
 
-    public abstract string symbolLookup(Entity entityIn);
-}
 
 public enum SymbolMappingTechnique : string
 {
