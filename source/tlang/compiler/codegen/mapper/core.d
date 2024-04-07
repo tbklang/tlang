aa1c19b6ed9207115631c3ae027d7dd0da72750b
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-04-07 12:30:43 +0200

    SymbolMappingTechnique
    - Documented

--- source/tlang/compiler/codegen/mapper/core.d
+++ source/tlang/compiler/codegen/mapper/core.d
@@ -6,10 +6,25 @@ import std.conv : to;
 import gogga;
 
 
-
-
+/** 
+ * Describes the mapping technique to use
+ *
+ * This describes which engine should be
+ * used to map entities to emittable
+ * names
+ */
 public enum SymbolMappingTechnique : string
 {
+    /**
+     * Uses a hash-based approach to
+     * generating names
+     */
     HASHMAPPER = "hashmapper",
+
+    /**
+     * Uses a more human-readable
+     * approach to generating
+     * names
+     */
     LEBANESE = "lebanese"
 }
