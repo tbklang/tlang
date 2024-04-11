8f5faf58f04ee614eddd736b15b0144f4a74df20
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-04-11 17:30:54 +0200

    Commands
    - Switched to new logging API

--- source/tlang/commandline/commands.d
+++ source/tlang/commandline/commands.d
@@ -198,18 +198,17 @@ struct compileCommand
         }
         catch(TError t)
         {
-            gprintln(t.msg, DebugType.ERROR);
+            ERROR(t.msg);
             exit(-1);
         }
         catch(ErrnoException e)
         {
-            /* TODO: Use gogga error */
-            writeln("Could not open source file "~sourceFile);
+            ERROR("Could not open source file "~sourceFile);
             exit(-2);
         }
         catch(Exception e)
         {
-            gprintln(e.msg, DebugType.ERROR);
+            ERROR(e.msg);
             exit(-1);
         }
     }
@@ -252,13 +251,12 @@ struct lexCommand
         }
         catch(TError t)
         {
-            gprintln(t.msg, DebugType.ERROR);
+            ERROR(t.msg);
             exit(-1);
         }
         catch(ErrnoException e)
         {
-            /* TODO: Use gogga error */
-            writeln("Could not open source file "~sourceFile);
+            ERROR("Could not open source file "~sourceFile);
             exit(-2);
         }
     }
@@ -303,13 +301,12 @@ struct parseCommand
         }
         catch(TError t)
         {
-            gprintln(t.msg, DebugType.ERROR);
+            ERROR(t.msg);
             exit(-1);
         }
         catch(ErrnoException e)
         {
-            /* TODO: Use gogga error */
-            writeln("Could not open source file "~sourceFile);
+            ERROR("Could not open source file "~sourceFile);
             exit(-2);
         }
     }
@@ -360,13 +357,12 @@ struct typecheckCommand
         }
         catch(TError t)
         {
-            gprintln(t.msg, DebugType.ERROR);
+            ERROR(t.msg);
             exit(-1);
         }
         catch(ErrnoException e)
         {
-            /* TODO: Use gogga error */
-            writeln("Could not open source file "~sourceFile);
+            ERROR("Could not open source file "~sourceFile);
             exit(-2);
         }
     }
