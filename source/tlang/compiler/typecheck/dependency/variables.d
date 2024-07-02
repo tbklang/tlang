b1791f99ab28c385f21e21032a8d072238533650
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2024-07-02 08:34:19 +0200

    AssignmentOf
    - Set the `name` field
    - Fixed constructor
    
    AssignmentTo
    
    - Set the `name` field
    - Fixed constructor

--- source/tlang/compiler/typecheck/dependency/variables.d
+++ source/tlang/compiler/typecheck/dependency/variables.d
@@ -102,16 +102,22 @@ public class VariableAssignmentNode : DNode
 
 public class AssignmentOf : DNode
 {
+    // FIXME: Update DNode super-type
+    // ... to not require this super-call
     this()
     {
-
+        super(null);
+        forceName("AssignmentOf");
     }
 }
 
 public class AssignmentTo : DNode
 {
+    // FIXME: Update DNode super-type
+    // ... to not require this super-call
     this()
     {
-        
+        super(null);
+        forceName("AssignmentTo");
     }
 }
