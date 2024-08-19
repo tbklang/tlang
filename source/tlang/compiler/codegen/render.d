ac9bf3603d2bb091a4fa06fc4d047e50d12ef37b
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2024-08-19 09:57:30 +0200

    Render
    - Added `tryRender(Instruction)`

--- source/tlang/compiler/codegen/render.d
+++ source/tlang/compiler/codegen/render.d
@@ -1,5 +1,7 @@
 module tlang.compiler.codegen.render;
 
+import tlang.compiler.codegen.instruction : Instruction;
+
 /** 
  * Any instruction which implements
  * this method can have a string
@@ -19,4 +21,31 @@ public interface IRenderable
      * representation
      */
     public string render();
+}
+
+/** 
+ * Attempts to render the given
+ * instruction. If the instruction
+ * supports the `IRenderable`
+ * interface then that will be
+ * used, otherwise the name of
+ * the instruction will be the
+ * fallback
+ *
+ * Params:
+ *   instr = the instruction
+ * Returns: the representation
+ */
+public string tryRender(Instruction instr)
+{
+    IRenderable r_i = cast(IRenderable)instr;
+
+    if(r_i is null)
+    {
+        return instr.classinfo.name;
+    }
+    else
+    {
+        return r_i.render();    
+    }
 }
