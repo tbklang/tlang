789b2a1c3c74c4c8398a8d6ad54545c46364f499
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2024-10-21 19:26:16 +0200

    DGen
    - Casting should wrap around when emitting `(<type>)(<instruction>)`

--- source/tlang/compiler/codegen/emit/dgen.d
+++ source/tlang/compiler/codegen/emit/dgen.d
@@ -600,7 +600,7 @@ public final class DCodeEmitter : CodeEmitter
                     emit ~= "("~typeTransform(castingTo)~")";
 
                     /* The expression being casted */
-                    emit ~= transform(uncastedInstruction);
+                    emit ~= "("~transform(uncastedInstruction)~")";
                 }
                 else
                 {
