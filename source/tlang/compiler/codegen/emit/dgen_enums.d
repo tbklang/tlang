4db04f17e8c9c5cb9bc2471300cba01ee72b861e
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2024-10-29 18:43:43 +0200

    DGen (enums)
    - Use `pool`

--- source/tlang/compiler/codegen/emit/dgen_enums.d
+++ source/tlang/compiler/codegen/emit/dgen_enums.d
@@ -21,28 +21,57 @@ private struct EnumNameStore
     }
 }
 
+
+
 // TODO: Move to seperate module
-public class EnumMapper
+// TODO: Is there _anyway_ to integrate
+// ... a `Pool!(E,V)` here?
+
+/** 
+ * The enumeration type
+ * mapper has the ability
+ * to consume a given
+ * enum type and the name
+ * of its member, afterwhich
+ * it then returns a name
+ * unique to that (Enum, name)
+ * pair
+ *
+ * This is used by the C
+ * emitter as enumeration
+ * types need to have
+ * completely unique 
+ * member names (in C)
+ */
+public final class EnumMapper
 {
+    import niknaks.containers : Pool;
+    private Pool!(EnumNameStore, Enum, false) _p;
     private size_t _roll;
-    private EnumNameStore[Enum] _s;
+    // private EnumNameStore[Enum] _s;
 
-    this()
-    {
-        
-    }
+    // TODO: Replace this with a Pool?
+    // private EnumNameStore* enter(Enum e)
+    // {
+    //     EnumNameStore* _es = e in _s;
+    //     if(_es is null)
+    //     {
+    //         this._s[e] = EnumNameStore();
+    //         return enter(e);
+    //     }
+    //     return _es;
+    // }
 
-    private EnumNameStore* enter(Enum e)
-    {
-        EnumNameStore* _es = e in _s;
-        if(_es is null)
-        {
-            this._s[e] = EnumNameStore();
-            return enter(e);
-        }
-        return _es;
-    }
-
+    /** 
+     * Returns the unique name
+     * for the enum and name
+     * pair
+     *
+     * Params:
+     *   e = the `Enum`
+     *   m = the member name
+     * Returns: the generated name
+     */
     public string getName(Enum e, string m)
     {
         scope(exit)
@@ -50,7 +79,8 @@ public class EnumMapper
             this._roll++;
         }
 
-        EnumNameStore* _es = enter(e);
+        // EnumNameStore* _es = enter(e);
+        EnumNameStore* _es = _p.pool(e);
         return _es.mapName(m, this._roll);
     }
 }
