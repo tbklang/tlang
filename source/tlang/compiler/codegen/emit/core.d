e2d964e499ba4076ad1aa6388377a03bddea8788
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-12-20 18:30:11 +0200

    CodeEmitter
    - Updated signature for `finalize()` (backported from `feature/comments`)

--- source/tlang/compiler/codegen/emit/core.d
+++ source/tlang/compiler/codegen/emit/core.d
@@ -150,10 +150,17 @@ public abstract class CodeEmitter
     public abstract void emit();
 
     /** 
-     * Finalizes the emitting process (only
-     * to be called after the `emit()` finishes)
+     * Finalizes the emitting process.
+     
+     * This is only to be called AFTER
+     * one has called `emit()`
+     *
+     * Returns: an `EmitResult`
+     * containing information
+     * about the successful
+     * emit process
      */
-    public abstract void finalize();
+    public abstract EmitResult finalize();
 
     /** 
      * Transforms or emits a single Instruction
