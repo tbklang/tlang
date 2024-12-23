52316c77f6bbd98a4056a868dca508b3e79020fc
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2024-12-23 16:24:29 +0200

    Compiler (core)
    - Return a `CompileResult`

--- source/tlang/compiler/core.d
+++ source/tlang/compiler/core.d
@@ -268,7 +268,7 @@ public class Compiler
     }
 
     /* Perform code emitting */
-    public void doEmit()
+    public CompileResult doEmit()
     {
         if(typeChecker is null)
         {
@@ -299,17 +299,20 @@ public class Compiler
         this.emitter = new DCodeEmitter(typeChecker, emitOutFile, config, mapper);
         emitter.emit(); // Emit the code
         emitOutFile.close(); // Flush (perform the write() syscall)
-        emitter.finalize(); // Call CC on the file containing generated C code
+        return emitter.finalize(); // Call CC on the file containing generated C code
     }
 
     /** 
      * Performs the compilation
      *
+     * Returns: A `CompileResult`
+     * containing information about
+     * the produced output
      * Throws: TError on any error
      * that may occur during any
      * of the compiler's stages
      */
-    public void compile()
+    public CompileResult compile()
     {
         /* Setup the lexer, perform the tokenization and obtain the tokens */
         doLex();
@@ -321,7 +324,7 @@ public class Compiler
         doTypeCheck();
 
         /* Perform code emitting */
-        doEmit();
+        return doEmit();
     }
 }
 
