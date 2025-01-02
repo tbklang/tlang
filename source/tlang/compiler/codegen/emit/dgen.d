108eea63a44b831cc24de3b64b9caa379512da7d
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-02 08:11:44 +0200

    DGen
    - Print out linking args

--- source/tlang/compiler/codegen/emit/dgen.d
+++ source/tlang/compiler/codegen/emit/dgen.d
@@ -1796,6 +1796,7 @@ int main()
 
             // Now link all object files (the `.o`'s) together
             // and perform linking
+            INFO("Linking args: ", args);
             Pid ccPID = spawnProcess(args);
             int code = wait(ccPID);
             total_l = watch.peek();
