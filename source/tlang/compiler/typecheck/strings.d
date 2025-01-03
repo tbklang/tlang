51f1a5205934be4d90faf4d51198aecc24a48f04
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-03 11:35:36 +0200

    String management
    - Work-in-progress

--- source/tlang/compiler/typecheck/strings.d
+++ source/tlang/compiler/typecheck/strings.d
@@ -6,7 +6,7 @@
 module tlang.compiler.typecheck.strings;
 
 import niknaks.containers : Pool;
-import tlang.compiler.symbols.expressions : StringExpression;
+import tlang.compiler.symbols.strings;
 
 public struct StringNode
 {
@@ -14,8 +14,27 @@ public struct StringNode
     private string _s;
     this(StringExpression s_expr)
     {
-        this._s = s_expr.getStringLiteral();
+        // this._s = s_expr.getStringLiteral();
     }
 }
 
 public alias StringPool = Pool!(StringNode, StringExpression);
+
+
+public struct StringNode2
+{
+    private string _data;
+    private Object _meta;
+
+    private this(string data)
+    {
+
+    }
+
+    // TODO: Implement hash and oequals
+}
+
+public struct StringManager
+{
+    
+}
