d38767132e677991c7421f1e5a982eea420e1547
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-03 11:38:24 +0200

    Expressions
    - Removed `StringExpression`

--- source/tlang/compiler/symbols/expressions.d
+++ source/tlang/compiler/symbols/expressions.d
@@ -7,26 +7,7 @@ import std.conv : to;
 import tlang.compiler.symbols.mcro : MStatementSearchable, MStatementReplaceable, MCloneable;
 import std.string : format;
 
-/* TODO: Look into arrays later */
-public final class StringExpression : Expression
-{
-    private string ztring;
 
-    this(string ztring)
-    {
-        this.ztring = ztring.length ? ztring[1..$-1] : "";
-    }
-
-    public string getStringLiteral()
-    {
-        return ztring;
-    }
-
-    public override string toString()
-    {
-        return format("\"%s\"", this.ztring);
-    }
-}
 
 public class OperatorExpression : Expression
 {
