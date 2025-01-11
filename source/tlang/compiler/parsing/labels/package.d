d6630adb824fb594c289b5d6fd01016fa8579ea5
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-11 16:09:58 +0200

    Package (labels)
    - Added public imports

--- source/tlang/compiler/parsing/labels/package.d
+++ source/tlang/compiler/parsing/labels/package.d
@@ -1,2 +1,4 @@
 module tlang.compiler.parsing.labels;
 
+public import tlang.compiler.parsing.labels.manager : LabelManager;
+public import tlang.compiler.parsing.labels.types : Label, LabelException;
