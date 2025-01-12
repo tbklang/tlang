361417a5104cf1da27bc1bd38b23b71bdb44a057
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2025-01-12 09:56:40 +0200

    CommentManager
    - Added `popComment()`

--- source/tlang/compiler/symbols/comments/manager.d
+++ source/tlang/compiler/symbols/comments/manager.d
@@ -8,8 +8,13 @@ module tlang.compiler.symbols.comments.manager;
 import tlang.compiler.lexer.core.tokens : Token;
 import tlang.compiler.symbols.comments : Comment;
 
+import std.container.slist : SList;
+import niknaks.functional : Optional;
+
 public final class CommentManager // TODO: Make a sruct
 {
+    private SList!(Comment) _stk;
+
     this()
     {
 
@@ -22,6 +27,19 @@ public final class CommentManager // TODO: Make a sruct
 
     public void pushComment(Comment comment)
     {
+        _stk.insertFront(comment);
+    }
 
+    public Optional!(Comment) popComment()
+    {
+        if(_stk.empty())
+        {
+            return Optional!(Comment).empty();
+        }
+
+        Comment c = _stk.front();
+        // import std.range : popFront;
+        _stk.linearRemoveElement(c);
+        return Optional!(Comment)(c);
     }
 }
