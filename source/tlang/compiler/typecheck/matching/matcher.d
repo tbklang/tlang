cfa81cc8d30598dfecb87af9c36b08e9d795d136
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-16 10:41:09 +0200

    Matcher
    - Added stub method

--- source/tlang/compiler/typecheck/matching/matcher.d
+++ source/tlang/compiler/typecheck/matching/matcher.d
@@ -1,4 +1,15 @@
 module tlang.compiler.typecheck.matching.matcher;
 
 import niknaks.functional : Optional;
+import tlang.compiler.typecheck.matching.types;
 
+// TODO: Add method here that takes in an interface and then scans
+// ... for classes that implement it via matching the method's type
+// ... signatures
+
+import tlang.compiler.symbols.containers : Clazz, Interface;
+
+public Optional!(Clazz[]) findImplementations(Interface i)
+{
+    return null;
+}
