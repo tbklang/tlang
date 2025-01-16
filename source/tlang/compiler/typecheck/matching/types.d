16f016e7fab376ccf9a24c1cfa58b578809b773e
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-16 12:02:55 +0200

    TypeSiganture
    - Added a unittest

--- source/tlang/compiler/typecheck/matching/types.d
+++ source/tlang/compiler/typecheck/matching/types.d
@@ -34,3 +34,20 @@ public struct TypeSignature
         return this._name == rhs.name() && this._tl == rhs.typeList();
     }
 }
+
+version(unittest)
+{
+    import tlang.compiler.symbols.typing.core : Type;
+    import tlang.compiler.symbols.typing.builtins : getBuiltInType;
+    // import tlang.compiler.typecheck.core : TypeChecker;
+}
+unittest
+{
+    Type[] t1_tl = [getBuiltInType(null, null, "ubyte"), getBuiltInType(null, null, "ubyte")];
+    TypeSignature t1 = TypeSignature("+", t1_tl);
+
+    Type[] t2_tl = [getBuiltInType(null, null, "ubyte"), getBuiltInType(null, null, "ubyte")];
+    TypeSignature t2 = TypeSignature("+", t2_tl);
+
+    assert(t1 == t2);
+}
