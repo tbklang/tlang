16289bcc4988796a0b96c7d638dbdce1450011c4
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-16 12:33:13 +0200

    TypeSignature
    - Added type comparison
    - Updated unittest

--- source/tlang/compiler/typecheck/matching/types.d
+++ source/tlang/compiler/typecheck/matching/types.d
@@ -1,6 +1,7 @@
 module tlang.compiler.typecheck.matching.types;
 
 import tlang.compiler.symbols.typing.core : Type;
+import tlang.compiler.typecheck.core : TypeChecker;
 
 /** 
  * Describes a type signature
@@ -9,11 +10,14 @@ import tlang.compiler.symbols.typing.core : Type;
  */
 public struct TypeSignature
 {
+    private TypeChecker _tc; // for type comparisons
+
     private string _name;
     private Type[] _tl;
 
-    this(string name, Type[] typeList)
+    package this(TypeChecker tc, string name, Type[] typeList)
     {
+        this._tc = tc;
         this._name = name;
         this._tl = typeList;
     }
@@ -31,7 +35,22 @@ public struct TypeSignature
 
     public bool opEquals(TypeSignature rhs)
     {
-        return this._name == rhs.name() && this._tl == rhs.typeList();
+        if(this._tl.length != rhs.typeList().length)
+        {
+            return false;
+        }
+
+        for(size_t i = 0; i < this._tl.length; i++)
+        {
+            Type this_t = this._tl[i];
+            Type rhs_t = rhs.typeList()[i];
+            if(!_tc.isSameType(this_t, rhs_t))
+            {
+                return false;
+            }
+        }
+
+        return this._name == rhs.name();
     }
 }
 
@@ -39,15 +58,25 @@ version(unittest)
 {
     import tlang.compiler.symbols.typing.core : Type;
     import tlang.compiler.symbols.typing.builtins : getBuiltInType;
-    // import tlang.compiler.typecheck.core : TypeChecker;
+    import tlang.compiler.typecheck.core : TypeChecker;
+    import tlang.compiler.core;
+    import std.stdio : File;
 }
+
+
 unittest
 {
+    string sourceFile = "source/tlang/testing/empty.t";
+    File outFile;
+    outFile.open("tlangout.c", "w");
+
+    Compiler compiler = new Compiler(gibFileData(sourceFile), sourceFile, outFile);
+    TypeChecker tc = new TypeChecker(compiler);
     Type[] t1_tl = [getBuiltInType(null, null, "ubyte"), getBuiltInType(null, null, "ubyte")];
-    TypeSignature t1 = TypeSignature("+", t1_tl);
+    TypeSignature t1 = TypeSignature(tc, "+", t1_tl);
 
     Type[] t2_tl = [getBuiltInType(null, null, "ubyte"), getBuiltInType(null, null, "ubyte")];
-    TypeSignature t2 = TypeSignature("+", t2_tl);
+    TypeSignature t2 = TypeSignature(tc, "+", t2_tl);
 
     assert(t1 == t2);
 }
