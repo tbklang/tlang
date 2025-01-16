cdebecf0eb07c2427cfafe6fbeb63351a53623d5
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-16 11:05:22 +0200

    Matcher
    - Added some more logic

--- source/tlang/compiler/typecheck/matching/matcher.d
+++ source/tlang/compiler/typecheck/matching/matcher.d
@@ -7,9 +7,46 @@ import tlang.compiler.typecheck.matching.types;
 // ... for classes that implement it via matching the method's type
 // ... signatures
 
-import tlang.compiler.symbols.containers : Clazz, Interface;
+import tlang.compiler.symbols.containers : Clazz, Interfaze;
 
-public Optional!(Clazz[]) findImplementations(Interface i)
+
+public Optional!(Clazz[]) findImplementations(TypeChecker tc, Interfaze i)
 {
-    return null;
+    // check first for super-interfaces and process those
+    // first; bottom of type-tree last
+    string[] superIs = i.superInterfaces();
+    if(superIs.length)
+    {
+        foreach(string super_i; superIs)
+        {
+            Type super_t = tc.getType(i, super_i);
+            // TODO: Check here that `super_t` refers to an interface type and throw error if not
+            Interfaze super_t_i = cast(Interfaze)super_t;
+            findImplementations(tc, super_t_i);
+        }
+
+    }
+
+    // i.
+    return Optional!(Clazz[]).empty();
+}
+
+import tlang.compiler.typecheck.core : TypeChecker;
+import tlang.compiler.symbols.data : Function;
+import tlang.compiler.symbols.typing.core : Type;
+public TypeSignature fromFunction(TypeChecker tc, Function f)
+{
+    Type[] tl;
+    import tlang.compiler.symbols.data : VariableParameter;
+    foreach(VariableParameter vp; f.getParams())
+    {
+        Type vp_t = tc.getType(f, vp.getType());
+        tl ~= vp_t;
+    }
+    return TypeSignature(tl);
+}
+
+unittest
+{
+    
 }
