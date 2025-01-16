31497af59bb85da0087ecfcd59841e4f54b8151f
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-01-16 10:32:47 +0200

    typo fix

--- source/tlang/compiler/typecheck/matching/package.d
+++ source/tlang/compiler/typecheck/matching/package.d
@@ -2,7 +2,7 @@
  * Structural typing mechanism
  *
  * Authors: Tristan Brice Velloza Kildaire (deavmi)
- * Date: 16th of Janurary 2025
+ * Date: 16th of January 2025
 module tlang.compiler.typecheck.matching;
  */
 
