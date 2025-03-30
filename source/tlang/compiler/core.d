d1d6dc81a4f8c819145cbb970fb81b9498291fa2
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-03-30 19:37:04 +0200

    Test cases
    - Moved around

--- source/tlang/compiler/core.d
+++ source/tlang/compiler/core.d
@@ -503,7 +503,9 @@ unittest
                         "source/tlang/testing/simple_string.t",
 
                         "source/tlang/testing/mixins/str.t",
-                        "source/tlang/testing/mixins/file.t"
+                        "source/tlang/testing/mixins/file.t",
+
+                        "source/tlang/testing/aliases/simple.t"
                         ];
     foreach(string testFile; testFiles)
     {
