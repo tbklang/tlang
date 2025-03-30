95f5c1c1fc454399920e5e31d0c10cb05b1c9ccb
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-03-30 19:38:42 +0200

    Compiler
    - Fixed path to alias test case

--- source/tlang/compiler/core.d
+++ source/tlang/compiler/core.d
@@ -584,7 +584,7 @@ unittest
                         "source/tlang/testing/universal_coerce/simple_coerce_literal_good_stdalo.t",
                         "source/tlang/testing/simple_function_return_type_check_good.t",
                         "source/tlang/testing/modules/a.t",
-                        "source/tlang/testing/simple_aliases.t"
+                        "source/tlang/testing/aliases/simple.t"
     ];
 
     foreach(string testFileGood; testFilesGood)
