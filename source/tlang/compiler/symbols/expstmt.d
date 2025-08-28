2cb3f6bda000d0233a098a504615282500aee315
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2025-08-28 19:22:29 +0200

    ExpressionStatement
    - Added

---
+++ source/tlang/compiler/symbols/expstmt.d
@@ -0,0 +1,24 @@
+module tlang.compiler.symbols.expstmt;
+
+import tlang.compiler.symbols.data : Statement;
+import tlang.compiler.symbols.expressions : Expression;
+
+// this is a non-expression, so, a normal statement
+// that contains an expression
+//
+// Examples are:
+// 1. standalone funciton calls
+// 2. i++
+public final class ExpressionStatement : Statement
+{
+    private Expression _e;
+    this(Expression exp)
+    {
+        this.e = exp;
+    }
+
+    public Expression getExpression()
+    {
+        return this._e;
+    }
+}
