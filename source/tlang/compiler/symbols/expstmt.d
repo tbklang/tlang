1546bd917943cf547cd6566316d23ae3fec09d2d
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2025-08-29 19:04:50 +0200

    ExpressionStatement
    - Implemented `toString()`
    - Set weighting to `2`

--- source/tlang/compiler/symbols/expstmt.d
+++ source/tlang/compiler/symbols/expstmt.d
@@ -2,6 +2,7 @@ module tlang.compiler.symbols.expstmt;
 
 import tlang.compiler.symbols.data : Statement;
 import tlang.compiler.symbols.expressions : Expression;
+import std.string : format;
 
 // this is a non-expression, so, a normal statement
 // that contains an expression
@@ -15,10 +16,18 @@ public final class ExpressionStatement : Statement
     this(Expression exp)
     {
         this._e = exp;
+
+        /* Weighted like any other statement */
+        this.weight = 2;
     }
 
     public Expression getExpression()
     {
         return this._e;
+    }
+
+    public override string toString()
+    {
+        return format("ExpressionStmt [e: %s]", _e);
     }
 }
