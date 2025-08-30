65fc5f8a924e3c1994e3fbe9701b8fc573ba4ce3
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2025-08-30 12:57:38 +0200

    Updates

--- source/tlang/compiler/symbols/expstmt.d
+++ source/tlang/compiler/symbols/expstmt.d
@@ -10,9 +10,19 @@ import std.string : format;
 // Examples are:
 // 1. standalone funciton calls
 // 2. i++
+
+/** 
+ * This is a non-expression, so, a normal
+ * statement that contains an expression
+ *
+ * Examples are:
+ * 1. standalone funciton calls
+ * 2. i++
+ */
 public final class ExpressionStatement : Statement
 {
     private Expression _e;
+
     this(Expression exp)
     {
         this._e = exp;
