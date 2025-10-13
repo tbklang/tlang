39b253877d9a61f8e5bf236314440c9537f4a679
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2025-10-13 17:16:14 +0200

    AliasDeclaration
    - Added support for `MStatementSearchable` and `MStatementReplaceable`

--- source/tlang/compiler/symbols/aliases.d
+++ source/tlang/compiler/symbols/aliases.d
@@ -3,11 +3,15 @@ module tlang.compiler.symbols.aliases;
 import tlang.compiler.symbols.data : Statement;
 import tlang.compiler.symbols.expressions : Expression;
 import std.string : format;
+import tlang.compiler.symbols.mcro : MStatementSearchable, MStatementReplaceable;
+
+// Debugging
+import tlang.misc.logging;
 
 /** 
  * A declaration of an alias expression
  */
-public final class AliasDeclaration : Statement
+public final class AliasDeclaration : Statement, MStatementSearchable, MStatementReplaceable
 {
     private string aliasName;
     private Expression aliasExpr;
@@ -32,5 +36,53 @@ public final class AliasDeclaration : Statement
     public override string toString()
     {
         return format("Alias [name: %s, expr: %s]", this.aliasName, this.aliasExpr);
+    }
+
+    public override Statement[] search(TypeInfo_Class clazzType)
+    {
+        /* List of returned matches */
+        Statement[] matches;
+
+        /* Are we (ourselves) of this type? */
+        if(clazzType.isBaseOf(this.classinfo))
+        {
+            matches ~= [this];
+        }
+
+        /* Recurse on expression */
+        MStatementSearchable curStmtCasted = cast(MStatementSearchable)aliasExpr;
+        if(curStmtCasted)
+        {
+            matches ~= curStmtCasted.search(clazzType);
+        }
+
+        return matches;
+    }
+
+    public override bool replace(Statement thiz, Statement that)
+    {
+        /* If we, the `AliasDeclaration`, are the `thiz` then we cannot perform replacement */
+        if(this == thiz)
+        {
+            return false;
+        }
+        /* Check if we should replace the `Expression` */
+        else if(thiz == aliasExpr)
+        {
+            auto newAliasExpr = cast(Expression)that;
+            if(newAliasExpr is null)
+            {
+                return false;
+            }
+
+            aliasExpr = newAliasExpr;
+            DEBUG("aliasdecl(", getName() ,") replace: thiz=", thiz, "that=", that);
+            return true;
+        }
+        /* Exhausted all possibilities */
+        else
+        {
+            return false;
+        }
     }
 }
