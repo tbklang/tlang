6b9302bd01ea59fdf430640c329b299e3da21fd0
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2026-01-04 11:52:17 +0200

    Messaging
    - Removed uneeded items

--- source/tlang/misc/messaging.d
+++ source/tlang/misc/messaging.d
@@ -11,52 +11,11 @@
  */
 module tlang.misc.messaging;
 
-import gogga;
+// FIXME: Make use of dlog and a custom transformer
 import dlog.basic : Level, FileHandler;
+
+import std.array : join;
 import std.stdio : stderr;
-
-/** 
- * The logger instance
- * shared amongst a single
- * thread (TLS)
- */
-private GoggaLogger logger;
-
-/**
- * Initializes a logger instance
- * per thread (TLS)
- */
-static this()
-{
-    logger = new GoggaLogger();
-
-    GoggaMode mode;
-
-    version(DBG_VERBOSE_LOGGING)
-    {
-        mode = GoggaMode.RUSTACEAN;
-    }
-    else
-    {
-        mode = GoggaMode.SIMPLE;
-    }
-
-    logger.mode(mode);
-
-    Level level;
-
-    version(DBG_DEBUG_LOGGING)
-    {
-        level = Level.DEBUG;
-    }
-    else
-    {
-        level = Level.INFO;
-    }
-
-    logger.setLevel(level);
-    logger.addHandler(new FileHandler(stderr));
-}
 
 byte[] WARN_COLOR = [27, '[', '3', '3', 'm'];
 byte[] ERROR_COLOR = [27, '[', '3', '1', 'm'];
@@ -65,7 +24,7 @@ byte[] TIP_COLOR = [27, '[', '3', '6', 'm'];
 
 byte[] RESET = [27, '[', 'm'];
 
-import std.array : join;
+
 
 // Use this for messages that are warnings
 public void warn(T...)(T[] args)
