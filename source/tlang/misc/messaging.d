bcbdf56efa6b81eb6fbb3c923f26bdf51cd56956
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2026-01-04 14:34:09 +0200

    Messaging
    - Added advanced logging mechanism to be able to automagically make nice messages

--- source/tlang/misc/messaging.d
+++ source/tlang/misc/messaging.d
@@ -15,7 +15,12 @@ module tlang.misc.messaging;
 import dlog.basic : Level, FileHandler;
 
 import std.array : join;
+import std.conv : to;
 import std.stdio : stderr;
+import std.string : format;
+
+import tlang.compiler.symbols.data : Entity, Module;
+import tlang.compiler.typecheck.resolution : Resolver;
 
 byte[] WARN_COLOR = [27, '[', '3', '3', 'm'];
 byte[] ERROR_COLOR = [27, '[', '3', '1', 'm'];
@@ -24,42 +29,129 @@ byte[] TIP_COLOR = [27, '[', '3', '6', 'm'];
 
 byte[] RESET = [27, '[', 'm'];
 
+// TODO: Move into niknaks
+// Generates short name of the classinfo.name
+// such that x.y.z -> z
+private string shortName(TypeInfo_Class ti)
+{
+    import std.string : split;
+    string shortName = ti.name.split(".")[$-1];
+    return shortName;
+}
+
+import niknaks.meta : isClassType;
+private string shortName(T)(T item)
+if
+(
+    isClassType!(T)()
+)
+{
+    return shortName(T.classinfo);
+}
+
+private string genToString(T)(T item, Resolver r = null)
+{
+    import std.traits : isAssignable, isIntegral;
+    static if(__traits(isSame, T, string))
+    {
+        return item;
+    }
+    // Transform any entity into the following shortstring:
+    //
+    // Entity "x" -> "x" in <module of x>
+    else static if(isAssignable!(Entity, T))
+    {
+        pragma(msg, "ENtity-based formatting active");
+
+        string entType = shortName(item);
+
+        // FIXME: Fix types without parenting
+        // assert(item.parentOf());
+        if(item.parentOf())
+        {
+            Module inMod = cast(Module)Resolver.findContainerOfType(Module.classinfo, item);
+            return format("%s '%s' in module %s", entType, item.getName(), inMod.getName());
+        }
+        else
+        {
+            return format("%s '%s'", entType, item.getName());
+        }
+    }
+    else static if(isIntegral!(T))
+    {
+        return format("%s", item);
+    }
+    else
+    {
+        return format("'%s'", to!(string)(item));
+    }
+}
+
+// Given any arguments this makes a string with
+// formatting automatically applied to arguments
+// of interest
+public string makeMessage(T...)(T args, Resolver r = null)
+{
+    string[] s_a;
+    static foreach(a; args)
+    {
+        s_a ~= genToString(a, r);
+    }
+    string s = join(s_a, " ");
+    return s;
+}
 
 
 // Use this for messages that are warnings
-public void warn(T...)(T[] args)
+public void warn(T...)(T args, string subSystem = "")
 {
-    stderr.write(WARN_COLOR);
-    string s = join(args, " ");
-    stderr.write(s);
-    stderr.write(RESET);
+    byte[] b;
+    b ~= WARN_COLOR;
+    b ~= "Warning ";
+    b ~= RESET;
+    b ~= makeMessage(args);
+    b ~= "\n";
+
+    stderr.rawWrite(b);
 }
 
 // Use this for any messages that are informative
 // tips
-public void tip(T...)(T[] args)
+public void tip(T...)(T args, string subSystem = "")
 {
-    stderr.write(TIP_COLOR);
-    string s = join(args, " ");
-    stderr.write(s);
-    stderr.write(RESET);
+    byte[] b;
+    b ~= TIP_COLOR;
+    b ~= "Tip ";
+    b ~= RESET;
+    b ~= makeMessage(args);
+    b ~= "\n";
+
+    stderr.rawWrite(b);
 }
 
 // Use this for any messages indicating an error
-public void error(T...)(T[] args)
+public void error(T...)(T args, string subSystem = "")
 {
-    stderr.write(ERROR_COLOR);
-    string s = join(args, " ");
-    stderr.write(s);
-    stderr.write(RESET);
+    byte[] b;
+    b ~= ERROR_COLOR;
+    b ~= "Error ";
+    b ~= RESET;
+    b ~= makeMessage(args);
+    b ~= "\n";
+
+    stderr.rawWrite(b);
 }
 
 // Use this for any messages indicating an informative
 // message
-public void info(T...)(T[] args)
+public void info(T...)(T args, string subSystem = "")
 {
-    stderr.write(INFO_COLOR);
-    string s = join(args, " ");
-    stderr.write(s);
-    stderr.write(RESET);
+    byte[] b;
+    b ~= INFO_COLOR;
+    b ~= "Info ";
+    b ~= RESET;
+    b ~= makeMessage(args);
+    b ~= "\n";
+
+    stderr.rawWrite(b);
 }
