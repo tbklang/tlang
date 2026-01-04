99659b30e1464b038d05d2522ca84267ba7c1632
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2026-01-04 11:51:27 +0200

    Messaging
    - Added messaging library

---
+++ source/tlang/misc/messaging.d
@@ -0,0 +1,106 @@
+/** 
+ * This provides all the routines to be used
+ * when user-oriented logging is to be done.
+ *
+ * User-oriented logging is defined as anything
+ * that would be important to let the user
+ * know about, hence debugging logs are not
+ * considered patt of this definition.
+ *
+ * Authors: Tristan Brice Velloza Kildaire
+ */
+module tlang.misc.messaging;
+
+import gogga;
+import dlog.basic : Level, FileHandler;
+import std.stdio : stderr;
+
+/** 
+ * The logger instance
+ * shared amongst a single
+ * thread (TLS)
+ */
+private GoggaLogger logger;
+
+/**
+ * Initializes a logger instance
+ * per thread (TLS)
+ */
+static this()
+{
+    logger = new GoggaLogger();
+
+    GoggaMode mode;
+
+    version(DBG_VERBOSE_LOGGING)
+    {
+        mode = GoggaMode.RUSTACEAN;
+    }
+    else
+    {
+        mode = GoggaMode.SIMPLE;
+    }
+
+    logger.mode(mode);
+
+    Level level;
+
+    version(DBG_DEBUG_LOGGING)
+    {
+        level = Level.DEBUG;
+    }
+    else
+    {
+        level = Level.INFO;
+    }
+
+    logger.setLevel(level);
+    logger.addHandler(new FileHandler(stderr));
+}
+
+byte[] WARN_COLOR = [27, '[', '3', '3', 'm'];
+byte[] ERROR_COLOR = [27, '[', '3', '1', 'm'];
+byte[] INFO_COLOR = [27, '[', '3', '2', 'm'];
+byte[] TIP_COLOR = [27, '[', '3', '6', 'm'];
+
+byte[] RESET = [27, '[', 'm'];
+
+import std.array : join;
+
+// Use this for messages that are warnings
+public void warn(T...)(T[] args)
+{
+    stderr.write(WARN_COLOR);
+    string s = join(args, " ");
+    stderr.write(s);
+    stderr.write(RESET);
+}
+
+// Use this for any messages that are informative
+// tips
+public void tip(T...)(T[] args)
+{
+    stderr.write(TIP_COLOR);
+    string s = join(args, " ");
+    stderr.write(s);
+    stderr.write(RESET);
+}
+
+// Use this for any messages indicating an error
+public void error(T...)(T[] args)
+{
+    stderr.write(ERROR_COLOR);
+    string s = join(args, " ");
+    stderr.write(s);
+    stderr.write(RESET);
+}
+
+// Use this for any messages indicating an informative
+// message
+public void info(T...)(T[] args)
+{
+    stderr.write(INFO_COLOR);
+    string s = join(args, " ");
+    stderr.write(s);
+    stderr.write(RESET);
+}
