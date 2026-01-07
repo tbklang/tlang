4fdcac62fb04510b061eff52e5b2e4ebb5d45b5f
Tristan Brice Velloza Kildaire <deavmi@redxen.eu>
2026-01-07 18:01:59 +0200

    TypeAlias
    - Defined

---
+++ source/tlang/compiler/symbols/remaps.d
@@ -0,0 +1,44 @@
+/**
+ * Type remapping
+ *
+ * Support for the type remapping AST type:
+ *
+ * ```
+ * type b = int;
+ * type a = b;
+ * ```
+ *
+ * These are named entities which store
+ * a pair of values (string, string);
+ * the first being the remapped type's name
+ * and the second being the type to refer
+ * to.
+ *
+ * Authors: Tristan Brice Velloza Kildaire (deavmi)
+ */
+module tlang.compiler.symbols.remaps;
+
+import tlang.compiler.symbols.typing.core : Type;
+import std.string : format;
+
+public final class TypeAlias : Type
+{
+    private string _mt;
+
+    this(string remappedName, string mappedTo)
+    {
+        super(remappedName);
+        this.weight = 2;
+        this._mt = mappedTo;
+    }
+
+    public override string toString()
+    {
+        return format("TypeAlias [name: %s, mappedTo: %s]", getName(), _mt);
+    }
+
+    public string getReferentType()
+    {
+        return this._mt;
+    }
+}
